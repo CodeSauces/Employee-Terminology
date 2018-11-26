a8ee3f1b4f38ebb10dfd770f10e52a5769730de6
LiaqatSaeed <liaqatsaeed007@gmail.com>
2018-11-26 20:28:12 +0500

    swipeDiv Changes checked in

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
-$(document).on('shown.bs.modal', function () {
-$('')
+// $(document).on('shown.bs.modal', function () {
+// $('')
 
-  },'.modal')
+//   },'.modal')
+$(function(){
+  $('.swap-div').click(function(){
+    var thatElement = $(this);
+    thatElement.next().toggleClass('height-0')
+    debugger;
+  })
+})
