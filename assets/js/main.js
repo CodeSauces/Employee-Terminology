fc26676cae77b83d4caa0dbaf98f7334c8033c80
LiaqatSaeed <liaqatsaeed007@gmail.com>
2018-11-28 23:10:40 +0500

    Datepicker Changes done

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,10 +53,50 @@ if(SwipDiv){
 
 }
 
-
   $('li.nav-item').click(function(){
     var thatElement = $(this);
     thatElement.parent().find('.selected-tab').removeClass('selected-tab');
     thatElement.addClass('selected-tab')
   });
+
+
+  $.fn.datepicker.dates['en'] = {
+    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    daysMin: ["일", "월", "화", "수", "목", "금", "토"],
+    months: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov",
+        "Dec"
+    ],
+    today: "Today",
+    clear: "Clear",
+    format: "yyyy-mm-dd",
+    titleFormat: "yyyy-MM",
+    /* Leverages same syntax as 'format' */
+    weekStart: 0
+};
+$('.datepicker').datepicker({
+    language: "en",
+});
+$('.datepicker').datepicker()
+    .on('show', function (e) {
+        $('.datepicker-days .next,.datepicker-days .prev').html('');
+        var prev = $('.datepicker-days .prev');
+        prev.attr('colspan', 2);
+        prev.next().attr('colspan', 3);
+        $('.datepicker').addClass('datepicker-open');
+        if($('#datepicker-dropdown').length < 1){
+          $('body').append('<div id="datepicker-dropdown"></div>');
+        }
+
+        $('.dropdown-menu.datepicker-open').css({
+            top:$('.datepicker-open').offset().top,
+        })
+    });
+$('.datepicker').datepicker()
+    .on('hide', function (e) {
+        $('.datepicker').removeClass('datepicker-open');
+        $('div#datepicker-dropdown').remove();
+    });
+
 })
